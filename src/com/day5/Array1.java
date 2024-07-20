92841055f2a66460595e63a1170c7557558925cf
Saurabh_Kumar_Gond <sunnygond619@gmail.com>
2024-07-20 20:27:18 +0530

    array1 and array2 programs has been created

---
+++ src/com/day5/Array1.java
@@ -0,0 +1,16 @@
+package com.day5;
+
+public class Array1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int y []= {10,30,40,50,60}; //declaration and initialization
+		for (int a:y) {
+			System.out.println(a);
+		}
+		
+		
+
+	}
+
+}
