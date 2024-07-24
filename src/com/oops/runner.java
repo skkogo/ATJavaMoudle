cbb42c6dca82a06dc972da0f8cfa4be9da3bcb68
Saurabh_Kumar_Gond <sunnygond619@gmail.com>
2024-07-25 02:18:53 +0530

    singleton and person classes were created with runner class

---
+++ src/com/oops/runner.java
@@ -0,0 +1,10 @@
+package com.oops;
+
+public class runner {
+
+	public static void main(String[] args) {
+		Person p1= new Person("Raj", 'm', "Lucknow","91 9988059600" );
+		System.out.println(p1.getAddress());
+	}
+
+}
