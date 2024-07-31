29cdf05f65e31338e800178d649c4f2c9f268c20
Saurabh_Kumar_Gond <sunnygond619@gmail.com>
2024-07-31 14:12:29 +0530

    Gson library added into project and json object created in CreateJobPOJO thro

--- src/com/oops3/ProblemsPOJO.java
+++ src/com/oops3/ProblemsPOJO.java
@@ -2,15 +2,17 @@ package com.oops3;
 
 public class ProblemsPOJO {
 	private int id;
-  private String  remark;
-public ProblemsPOJO(int id, String remark) {
-	super();
-	this.id = id;
-	this.remark = remark;
-}
-@Override
-public String toString() {
-	return "ProblemsPOJO [id=" + id + ", remark=" + remark + "]";
-}
+	private String remark;
+
+	public ProblemsPOJO(int id, String remark) {
+		super();
+		this.id = id;
+		this.remark = remark;
+	}
+
+	@Override
+	public String toString() {
+		return "ProblemsPOJO [id=" + id + ", remark=" + remark + "]";
+	}
 
 }
--- src/com/oops3/Runner.java
+++ src/com/oops3/Runner.java
@@ -1,15 +1,26 @@
 package com.oops3;
 
+import com.google.gson.Gson;
+
 public class Runner {
-public static void main(String[] args) {
-	ProblemsPOJO[] prob= new ProblemsPOJO[2];
-	prob[0] = new ProblemsPOJO(7, "Audio Jack is not working");
-	prob [1] = new ProblemsPOJO(55, "Screen is broken");
-	CustomerProductPOJO customerProd= new CustomerProductPOJO("5667456", "12345678801234", "09876543210987", "23456789012345", "759588", 890009, 8);
-	CustomerAddressPOJO customerAddress = new CustomerAddressPOJO("788","Shrishti Apartment", "Kursi Road", "RK School", "lucknow", "226021", "India", "UttarPradesh");
-	CustomerPOJO customer=new CustomerPOJO("Saurabh", "Kumar", "899987898", "9899998999", "abdk@gmail.com", "samjjj@gmail.com");
-	CreateJobPOJO job = new CreateJobPOJO(55, 877, 76, 6, customer, customerAddress, customerProd, prob);
-	System.out.println(job);
-}
+	public static void main(String[] args) {
+		ProblemsPOJO[] prob = new ProblemsPOJO[2];
+		prob[0] = new ProblemsPOJO(7, "Audio Jack is not working");
+		prob[1] = new ProblemsPOJO(55, "Screen is broken");
+
+		CustomerProductPOJO customerProd = new CustomerProductPOJO("5667456", "12345678801234", "09876543210987",
+				"23456789012345", "759588", 890009, 8);
+		CustomerAddressPOJO customerAddress = new CustomerAddressPOJO("788", "Shrishti Apartment", "Kursi Road",
+				"RK School", "lucknow", "226021", "India", "UttarPradesh");
+		CustomerPOJO customer = new CustomerPOJO("Saurabh", "Kumar", "899987898", "9899998999", "abdk@gmail.com",
+				"samjjj@gmail.com");
+		CreateJobPOJO job = new CreateJobPOJO(55, 877, 76, 6, customer, customerAddress, customerProd, prob);
+		Gson gson = new Gson();
+		String GData = gson.toJson(job);
+
+		System.out.println(job);
+		System.out.println("");
+		System.out.println(GData);
+	}
 
 }
